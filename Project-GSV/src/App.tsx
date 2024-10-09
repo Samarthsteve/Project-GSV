58f3b32df1b9f396c2ffd0e8e40a2a3620f1ffe9
Samarth Kotian <samarth9190sasteve@gmail.com>
2024-10-09 03:18:11 +0000

    Navbar Completed

--- Project-GSV/src/App.tsx
+++ Project-GSV/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
-import Footer from './components/Footer'
+// import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
-      <Footer />
+      <Navbar />
+      {/* <Footer /> */}
     </>
   )
 }
---
+++ Project-GSV/src/components/Navbar.tsx
@@ -0,0 +1,70 @@
+import '../styles/Navbar.css';
+
+import { useState } from 'react';
+
+export default function Navbar(){
+   const [isTriggerClicked, setTrigger] = useState(false);
+   
+   return (
+      <header className="header">
+         <nav className="nav container">
+            <div className="nav__data">
+               <a href="#" className="nav__logo">
+                  Gnyana Sudha Vidaylaya
+               </a>
+
+            <div className={`nav__toggle ${isTriggerClicked ? "show-icon" : ""}`} id="nav-toggle" onClick={() => setTrigger(!isTriggerClicked)}>
+                  <i className="ri-menu-line nav__burger"></i>
+                  <i className="ri-close-line nav__close"></i>
+               </div>
+            </div>
+
+            <div className={`nav__menu ${isTriggerClicked ? "show-menu" : ""}`} id="nav-menu">
+               <ul className="nav__list">
+                  <li>
+                     <a href="#" className="nav__link">
+                        Home
+                     </a>
+                  </li>
+
+                  <li>
+                     <a href="#" className="nav__link">
+                        About Us
+                     </a>
+                  </li>
+
+                  <li>
+                     <a href="#" className="nav__link">
+                        Timeline
+                     </a>
+                  </li>
+
+                  <li>
+                     <a href="#" className="nav__link">
+                        Our Alumini
+                     </a>
+                  </li>
+
+                  <li>
+                     <a href="#" className="nav__link">
+                        Events
+                     </a>
+                  </li>
+
+                  <li>
+                     <a href="#" className="nav__link">
+                        Gallery
+                     </a>
+                  </li>
+
+                  <li>
+                     <a href="#" className="nav__link">
+                        Contact Us
+                     </a>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+      </header>
+   );
+}
